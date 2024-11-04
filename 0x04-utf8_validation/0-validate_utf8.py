8c6f96550ef4dbc4eb1fc617bdf7301f99708cff
Precious <eseprecious934@gmail.com>
2024-11-04 00:38:19 +0000

    documented

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def validUTF8(data):
+    """
+    Check if the given data represents valid UTF-8 encoding.
+
+    Args:
+        data (List[int]): A list of integers where each integer represents one byte of data.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, otherwise False.
+    """
     expected_bytes = 0
     for num in data:
         byte = num & 0xFF
